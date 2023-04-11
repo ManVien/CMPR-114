7ed6d3ccd30e435bbc2bb0555de98b7987a3722a
manvien <leblue208@gmail.com>
2023-04-10 17:54:44 -0700

    update

--- Week 6/Class Exercise 4 Part 2/Challenge Exercise 1.py
+++ Week 6/Class Exercise 4 Part 2/Challenge Exercise 1.py
@@ -5,10 +5,15 @@
 def main():
     # Get a distance in kilometers.
     kilometers = float(input('Enter a distance in kilometers: '))
+    miles = get_conversion(kilometers)
+    show_conversion(miles)
 
+def get_conversion(kilometers):
     # Convert distance to miles.
     miles = kilometers * 0.6214
+    return miles
 
+def show_conversion(miles):
     # Display the distance in miles.
     print(f'The distance in miles is {miles:.2f}.')
 
--- Week 6/Class Exercise 4 Part 2/Challenge Exercise 2.py
+++ Week 6/Class Exercise 4 Part 2/Challenge Exercise 2.py
@@ -6,10 +6,11 @@
 # and the total annual cost of these expenses.
 
 def main():
-
-    total_monthly_cost = 0.0
-    total_annual_cost = 0.0
-
+    loan, insurance, gas, oil, tires, maintenance = get_input()
+    total_monthly, total_annual = calculate_cost(loan, insurance, gas, oil, tires, maintenance)
+    display_cost(total_monthly, total_annual)
+   
+def get_input():
     # Get monthly loan payment.
     loan_payment = float(input('Enter monthly loan payment: $'))
 
@@ -28,17 +29,26 @@ def main():
     # Get monthly maintenance cost.
     maintenance_cost = float(input('Enter monthly maintenance cost: $'))
 
+    return loan_payment, insurance_cost, gas_cost, oil_cost, tires_cost, maintenance_cost
+
+def calculate_cost(loan, insurance, gas, oil, tires, maintenance):
+    total_monthly_cost = 0.0
+    total_annual_cost = 0.0
+
     # Calculate the total monthly cost of these expenses.
-    total_monthly_cost = (loan_payment + insurance_cost + gas_cost + 
-                          oil_cost + tires_cost + maintenance_cost)
+    total_monthly_cost = (loan + insurance + gas + 
+                          oil + tires + maintenance)
     
     # Calculate the total annual cost of these expenses.
     total_annual_cost = total_monthly_cost * 12
 
+    return total_monthly_cost, total_annual_cost
+
+def display_cost(total_monthly, total_annual):
     # Display the total monthly cost and
     # the total annual cost of these expenses.
-    print(f'The total monthly cost is ${total_monthly_cost:,.2f}.\n'
-          f'The total annual cost is ${total_annual_cost:,.2f}.')
+    print(f'The total monthly cost is ${total_monthly:,.2f}.\n'
+          f'The total annual cost is ${total_annual:,.2f}.')
 
 # Call the main function.
 main()
